0ac6a908e0c26f6cdff664a2d2f051161cf1b8f3
erwan-legoff <erwan.le-goff@etu.umontpellier.fr>
2024-10-12 11:48:55 +0200

    JpaAudit activated, private properties, UUID and LocalDateTime

--- src/main/java/fr/erwil/Spricture/File/FileRepository.java
+++ src/main/java/fr/erwil/Spricture/File/FileRepository.java
@@ -2,5 +2,5 @@ package fr.erwil.Spricture.File;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository extends JpaRepository<File, Long> {
+public interface FileRepository extends JpaRepository<Medium, Long> {
 }
--- src/main/java/fr/erwil/Spricture/SprictureApplication.java
+++ src/main/java/fr/erwil/Spricture/SprictureApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //exclude = {DataSourceAutoConfiguration.class}
 @RestController
+@EnableJpaAuditing
 @SpringBootApplication()
 public class SprictureApplication {
 
