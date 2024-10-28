82917737dcc0f11a4c5f7d7f67659d2a7ec0fdb9
erwan-legoff <erwan.le-goff@etu.umontpellier.fr>
2024-10-28 22:09:27 +0100

    Now can save with a default root file in the appplication.properties

--- src/main/java/fr/erwil/Spricture/Tools/FileStorage/UuidFileStorageSimple.java
+++ src/main/java/fr/erwil/Spricture/Tools/FileStorage/UuidFileStorageSimple.java
@@ -1,22 +1,57 @@
 package fr.erwil.Spricture.Tools.FileStorage;
 
+import fr.erwil.Spricture.Exceptions.UuidFileStorage.FileAlreadyExistsException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
 @Service
 public class UuidFileStorageSimple implements IUuidFileStorage{
-    @Override
-    public boolean save(File file, UUID uuid) {
-        return false;
+    private final Path root;
+
+    private Path getPath(UUID uuid){
+        return Paths.get(this.root.toString(), uuid.toString());
+    }
+    public UuidFileStorageSimple(@Value("${file.storage.location}") String storageLocation) {
+        this.root = Paths.get(storageLocation).toAbsolutePath().normalize();
+
+        try {
+            Files.createDirectories(this.root);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not create storage directory", e);
+        }
     }
 
     @Override
-    public File read(UUID uuid) {
+    public boolean save(MultipartFile file, UUID uuid) throws IOException, FileAlreadyExistsException {
+        Path targetPath = this.getPath(uuid);
+        if(Files.exists(targetPath)) throw new FileAlreadyExistsException("A file already exists in " + targetPath);
+
+
+        Files.copy(file.getInputStream(), targetPath);
+
+        return true;
+    }
+
+    @Override
+    public InputStream read(UUID uuid) {
+
+
         return null;
     }
 
+    @Override
+    public void delete(UUID uuid) {
 
+    }
 }
 
