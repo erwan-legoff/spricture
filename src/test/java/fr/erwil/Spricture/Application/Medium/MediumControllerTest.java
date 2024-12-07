be774d418855aefec51a73c6a03a075f2c34fd2a
erwil <rwan.legoff@gmail.com>
2024-12-07 09:15:00 +0100

    Adding the test property (WIP)

--- src/test/java/fr/erwil/Spricture/Application/Medium/MediumControllerTest.java
+++ src/test/java/fr/erwil/Spricture/Application/Medium/MediumControllerTest.java
@@ -1,4 +1,7 @@
 package fr.erwil.Spricture.Application.Medium;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(MediumController.class)
 public class MediumControllerTest {
 }
