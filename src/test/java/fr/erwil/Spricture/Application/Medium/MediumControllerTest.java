7c0811a411aae3d6cc51fb0286dc022de096773e
erwil <rwan.legoff@gmail.com>
2024-12-08 18:01:27 +0100

    First test OK

--- src/test/java/fr/erwil/Spricture/Application/Medium/MediumControllerTest.java
+++ src/test/java/fr/erwil/Spricture/Application/Medium/MediumControllerTest.java
@@ -1,7 +1,62 @@
 package fr.erwil.Spricture.Application.Medium;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import fr.erwil.Spricture.Application.Medium.Dtos.GetMediumDto;
+import fr.erwil.Spricture.Exceptions.MediumNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@WebMvcTest(MediumController.class)
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class) // say to Spring that I use Mockito
 public class MediumControllerTest {
+    @InjectMocks
+    MediumController mediumController;
+
+    @Mock
+    IMediumService mediumService; // The service is needed for the controller
+
+    private static GetMediumDto dtoWithId(UUID uuid) {
+        return Mockito.argThat(dto -> dto.getId().equals(uuid));
+    }
+
+
+
+    @Test
+    void getMediumReturnsTheFileItReceivedAndSucceed() throws IOException {
+
+
+
+        UUID uuid = new UUID(1,1);
+        String fileName = uuid.toString();
+        Path tempFile = Files.createTempFile(fileName,"");
+        Files.writeString(tempFile, "test");
+
+        Mockito.doReturn(tempFile).when(mediumService).getMediumResource(dtoWithId(uuid));
+        ResponseEntity<Resource> result = mediumController.getMedium(fileName);
+
+        Mockito.verify(mediumService, Mockito.times(1)).getMediumResource(dtoWithId(uuid));
+        Assertions.assertNotNull(result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+
+        String fileContentExpected = Files.readString(tempFile);
+        String actualFileContent = new String(result.getBody().getInputStream().readAllBytes());
+        assertEquals(fileContentExpected, actualFileContent);
+
+
+        Files.deleteIfExists(tempFile);
+    }
+
 }
---
+++ src/test/java/fr/erwil/Spricture/Application/Medium/MediumServiceTest.java
@@ -0,0 +1,13 @@
+package fr.erwil.Spricture.Application.Medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MediumServiceTest {
+
+    @Test
+    void getMediumResourceShouldReturnSomething() {
+
+    }
+}
