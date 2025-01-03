e662095f48f61838835e83b189d1d45f3620d2e9
erwil <rwan.legoff@gmail.com>
2025-01-03 21:31:32 +0100

    Correction du path et ajout de log

--- src/main/java/fr/erwil/Spricture/Exceptions/GlobalExceptionsHandler.java
+++ src/main/java/fr/erwil/Spricture/Exceptions/GlobalExceptionsHandler.java
@@ -2,20 +2,26 @@ package fr.erwil.Spricture.Exceptions;
 
 import fr.erwil.Spricture.Exceptions.Medium.MediumNotFoundException;
 import fr.erwil.Spricture.Exceptions.Medium.MediumProcessingException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionsHandler {
 
 
+    private static final Logger log = LogManager.getLogger(GlobalExceptionsHandler.class);
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handleBaseException(BaseException exception, WebRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(exception, request.getContextPath());
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        log.error("BaseException at {}: {}", path, exception.getMessage(), exception);
+        ErrorResponse errorResponse = new ErrorResponse(exception, path);
         return new ResponseEntity<>(errorResponse, exception.getHttpStatus());
     }
 
@@ -23,8 +29,10 @@ public class GlobalExceptionsHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception exception, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        log.error("BaseException at {}: {}", path, exception.getMessage(), exception);
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        ErrorResponse errorResponse = new ErrorResponse(status, exception, request.getContextPath());
+        ErrorResponse errorResponse = new ErrorResponse(status, exception, path);
         return new ResponseEntity<>(errorResponse, status);
     }
 
