6acc2c4a10d0bcaedc14f5dbfc64915b3c49239a
erwil <rwan.legoff@gmail.com>
2025-01-28 23:43:54 +0100

    wip spring security initialized

---
+++ src/main/java/fr/erwil/Spricture/Configuration/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package fr.erwil.Spricture.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+}
