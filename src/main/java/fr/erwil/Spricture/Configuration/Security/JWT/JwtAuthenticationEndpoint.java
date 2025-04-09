8209814eff08190c6d1988de0607d93ad208244c
erwil <rw4nit0@gmail.com>
2025-04-09 17:17:12 +0200

    Init JwtAuthenticationEndpoint

--- src/main/java/fr/erwil/Spricture/Configuration/Security/JWT/JwtAuthenticationEndpoint.java
+++ src/main/java/fr/erwil/Spricture/Configuration/Security/JWT/JwtAuthenticationEndpoint.java
@@ -1,4 +1,20 @@
 package fr.erwil.Spricture.Configuration.Security.JWT;
 
-public class JwtAuthenticationEndpoint {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEndpoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+
+    }
 }
