d7ffc6b63444c58137676ada5d7613e6e0265b1a
erwil <rw4nit0@gmail.com>
2025-04-11 18:00:52 +0200

    make sure not to deleteAllFiles in production

--- src/main/java/fr/erwil/Spricture/SprictureApplication.java
+++ src/main/java/fr/erwil/Spricture/SprictureApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,6 +47,7 @@ public class SprictureApplication {
 	}
 
 	@Bean
+	@Profile("dev")
 	public CommandLineRunner applicationInitialization(IUuidFileStorage fileStorage){
 		return (args) -> {
 			fileStorage.deleteAll();
