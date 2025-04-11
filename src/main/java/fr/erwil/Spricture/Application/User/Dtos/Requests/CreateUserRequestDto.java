1414747f93043e75649784945a317d1382693e6c
erwil <rw4nit0@gmail.com>
2025-04-11 17:54:57 +0200

    now a more moder dto with validation

--- src/main/java/fr/erwil/Spricture/Application/User/Dtos/Requests/CreateUserRequestDto.java
+++ src/main/java/fr/erwil/Spricture/Application/User/Dtos/Requests/CreateUserRequestDto.java
@@ -1,45 +1,15 @@
 package fr.erwil.Spricture.Application.User.Dtos.Requests;
 
 import fr.erwil.Spricture.Application.User.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public class CreateUserRequestDto {
-    private final String pseudo, email, rawPassword, name, lastName;
-    private final UserRole role;
-
-    public CreateUserRequestDto(String pseudo, String email, String rawPassword, String name, String lastName, UserRole role) {
-        this.pseudo = pseudo;
-        this.email = email;
-        this.rawPassword = rawPassword;
-        this.name = name;
-        this.lastName = lastName;
-        this.role = role;
-    }
-
-    public String getPseudo() {
-        return pseudo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getRawPassword() {
-        return rawPassword;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
-    }
-}
+public record CreateUserRequestDto(
+        @NotBlank @Size(min = 3, max = 30) String pseudo,
+        @NotBlank @Email String email,
+        @NotBlank @Size(min = 6) String rawPassword,
+        @NotBlank String name,
+        @NotBlank String lastName,
+        UserRole role
+) {}
