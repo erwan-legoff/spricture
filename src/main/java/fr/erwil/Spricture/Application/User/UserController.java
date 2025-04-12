fae0094f09fbe3beedd274bcf8d4e4749a6800a6
erwil <rw4nit0@gmail.com>
2025-04-12 23:31:15 +0200

    user controller initialized

---
+++ src/main/java/fr/erwil/Spricture/Application/User/UserController.java
@@ -0,0 +1,26 @@
+package fr.erwil.Spricture.Application.User;
+
+import fr.erwil.Spricture.Application.User.Dtos.Requests.CreateUserRequestDto;
+import fr.erwil.Spricture.Application.User.Dtos.Responses.CreateUserResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    ResponseEntity<CreateUserResponseDto> createUser(CreateUserRequestDto requestDto){
+        CreateUserResponseDto responseDto = userService.create(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(responseDto);
+
+    }
+}
