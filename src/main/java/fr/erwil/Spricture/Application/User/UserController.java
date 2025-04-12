0a4d3a7b8092d397c0d987df22a736df63d45f97
erwil <rw4nit0@gmail.com>
2025-04-13 01:01:59 +0200

    now can create user

--- src/main/java/fr/erwil/Spricture/Application/User/UserController.java
+++ src/main/java/fr/erwil/Spricture/Application/User/UserController.java
@@ -2,9 +2,11 @@ package fr.erwil.Spricture.Application.User;
 
 import fr.erwil.Spricture.Application.User.Dtos.Requests.CreateUserRequestDto;
 import fr.erwil.Spricture.Application.User.Dtos.Responses.CreateUserResponseDto;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +19,7 @@ public class UserController {
     }
 
     @PostMapping("/user")
-    ResponseEntity<CreateUserResponseDto> createUser(CreateUserRequestDto requestDto){
+    ResponseEntity<CreateUserResponseDto> createUser(@RequestBody @Valid CreateUserRequestDto requestDto){
         CreateUserResponseDto responseDto = userService.create(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(responseDto);
