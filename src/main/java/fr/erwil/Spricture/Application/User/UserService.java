9fd7126166a6ce5f07285720355f5955d42209a0
erwil <rw4nit0@gmail.com>
2025-04-23 22:08:40 +0200

    WIP : now can send email when user is created

--- src/main/java/fr/erwil/Spricture/Application/User/UserService.java
+++ src/main/java/fr/erwil/Spricture/Application/User/UserService.java
@@ -4,6 +4,7 @@ import fr.erwil.Spricture.Application.User.Dtos.Adapters.CreateUserAdapter;
 import fr.erwil.Spricture.Application.User.Dtos.Requests.CreateUserRequestDto;
 import fr.erwil.Spricture.Application.User.Dtos.Responses.CreateUserResponseDto;
 import fr.erwil.Spricture.Exceptions.User.UserCreationException;
+import fr.erwil.Spricture.Tools.Mail.MailService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +13,22 @@ public class UserService implements IUserService {
 
     private final IUserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final MailService mailService;
 
-    public UserService(IUserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(IUserRepository userRepository, PasswordEncoder passwordEncoder, MailService mailService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.mailService = mailService;
     }
 
     @Override
     public CreateUserResponseDto create(CreateUserRequestDto user) {
         try {
             User userToCreate = CreateUserAdapter.getUser(user,passwordEncoder.encode(user.getRawPassword()));
+            // TODO : Create a email validation process
+            mailService.sendSimpleMessage(userToCreate.getEmail(),"Validate your email address.", "Please click on this link to validate your email address.");
             userRepository.save(userToCreate);
+            mailService.sendSimpleMessage(userToCreate.getEmail(),"Account Creation Success !", "Your account has been successfully created !");
             return CreateUserResponseDto.builder().userCreated(true).build();
         } catch (Exception e) {
             throw new UserCreationException("Error while creating user", e);
