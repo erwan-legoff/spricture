1dd1215d6f7f569453be87eb3db8fe40c539b3bc
erwil <rw4nit0@gmail.com>
2025-04-29 00:16:59 +0200

    now can find by owner id

--- src/main/java/fr/erwil/Spricture/Application/Medium/IMediumRepository.java
+++ src/main/java/fr/erwil/Spricture/Application/Medium/IMediumRepository.java
@@ -2,7 +2,9 @@ package fr.erwil.Spricture.Application.Medium;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IMediumRepository extends JpaRepository<Medium, UUID> {
+    List<Medium> findByOwnerId(Long ownerId);
 }
