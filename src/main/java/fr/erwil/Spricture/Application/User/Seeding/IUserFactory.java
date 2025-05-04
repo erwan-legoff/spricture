66fbc7ab6be72e79979dd1e5dcb3f9af715ea1ae
erwil <rw4nit0@gmail.com>
2025-05-04 16:19:32 +0200

    Now can create a random user with an arbitrary status

--- src/main/java/fr/erwil/Spricture/Application/User/Seeding/IUserFactory.java
+++ src/main/java/fr/erwil/Spricture/Application/User/Seeding/IUserFactory.java
@@ -1,11 +1,14 @@
 package fr.erwil.Spricture.Application.User.Seeding;
 
 import fr.erwil.Spricture.Application.User.User;
+import fr.erwil.Spricture.Application.User.UserRole;
+import fr.erwil.Spricture.Application.User.UserStatus;
 
 import java.util.List;
 
 public interface IUserFactory {
     User getRandomUser();
+    User getRandomUser(UserStatus status);
     User getUserUser();
     User getAdminUser();
     List<User> getManyRandomUsers(int number);
