3baaf6f62665e4c54f60af1ce3a41b3b1257a452
erwil <rw4nit0@gmail.com>
2025-05-04 16:37:15 +0200

    refactoring controller and adding block etc

--- src/main/java/fr/erwil/Spricture/Application/User/UserController.java
+++ src/main/java/fr/erwil/Spricture/Application/User/UserController.java
@@ -7,38 +7,46 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
-
-
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @PostMapping("/user")
-    ResponseEntity<CreateUserResponseDto> createUser(@RequestBody @Valid CreateUserRequestDto requestDto){
-        CreateUserResponseDto responseDto = userService.create(requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(responseDto);
-
+    @PostMapping
+    public ResponseEntity<CreateUserResponseDto> create(@RequestBody @Valid CreateUserRequestDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(dto));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping("/user")
-    ResponseEntity<List<GetUserResponseDto>> getUsers(){
-        var responseDto = userService.getMany();
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(responseDto);
-
+    @GetMapping
+    public ResponseEntity<List<GetUserResponseDto>> getAll() {
+        return ResponseEntity.ok(userService.getMany());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/{id}/validate")
+    public ResponseEntity<Boolean> validate(@PathVariable long id) {
+        return ResponseEntity.ok(userService.validateUserAccount(id));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/{id}/block")
+    public ResponseEntity<Boolean> block(@PathVariable long id) {
+        return ResponseEntity.ok(userService.block(id));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/{id}/unblock")
+    public ResponseEntity<Boolean> unblock(@PathVariable long id) {
+        return ResponseEntity.ok(userService.unblock(id));
+    }
 }
+
