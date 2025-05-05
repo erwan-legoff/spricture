18f555de9e8a29c406e43070cb91a3f0b1f44ed6
erwil <rw4nit0@gmail.com>
2025-05-05 09:53:25 +0200

    cleaner lomboke

--- src/main/java/fr/erwil/Spricture/Configuration/Security/JWT/SecurityJwtProperties.java
+++ src/main/java/fr/erwil/Spricture/Configuration/Security/JWT/SecurityJwtProperties.java
@@ -5,18 +5,16 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "security.jwt")
 public class SecurityJwtProperties {
-    @Getter
-    @Setter
+
     private String secretKey;
 
-    @Setter
-    @Getter
     private long loginExpirationMilliseconds = 1000 * 60 * 60;
-    @Setter
-    @Getter
+
     private long validateExpirationMilliseconds = 1000 * 60 * 10;
 
 }
