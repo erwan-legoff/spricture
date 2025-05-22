3aa71056fb01303a9f8f7846971d2d0a11895a8b
erwil <rwan.legoff@gmail.com>
2025-05-22 17:08:06 +0200

    cleaner DTO with record + lombok

--- src/main/java/fr/erwil/Spricture/Application/User/Dtos/Responses/GetMeResponseDto.java
+++ src/main/java/fr/erwil/Spricture/Application/User/Dtos/Responses/GetMeResponseDto.java
@@ -1,23 +1,14 @@
 package fr.erwil.Spricture.Application.User.Dtos.Responses;
 
-import fr.erwil.Spricture.Application.User.UserRole;
-import fr.erwil.Spricture.Application.User.UserStatus;
-import lombok.AllArgsConstructor;
+
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
-@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class GetMeResponseDto {
-    private Long id;
-    private String pseudo;
-    private String name;
-    private String lastName;
-    private String email;
-    private String salt;
-}
+public record GetMeResponseDto(
+        Long id,
+        String pseudo,
+        String name,
+        String lastName,
+        String email,
+        String salt
+) {}
