312ed1f8c1c79296173904e57b34340004b31c78
erwil <rw4nit0@gmail.com>
2025-06-04 10:25:06 +0200

    can now write to blackbaze

--- src/main/java/fr/erwil/Spricture/Tools/FileStorage/S3/UUIDFileStorageS3.java
+++ src/main/java/fr/erwil/Spricture/Tools/FileStorage/S3/UUIDFileStorageS3.java
@@ -3,28 +3,36 @@ package fr.erwil.Spricture.Tools.FileStorage.S3;
 import fr.erwil.Spricture.Exceptions.UuidFileStorage.UploadException;
 import fr.erwil.Spricture.Tools.FileStorage.IUuidFileStorage;
 import fr.erwil.Spricture.Exceptions.UuidFileStorage.FileAlreadyExistsException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
-import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
+
 @Primary
 @Qualifier("s3")
 @Service
 public class UUIDFileStorageS3 implements IUuidFileStorage {
+    private static final Logger log = LogManager.getLogger(UUIDFileStorageS3.class);
     private final S3Client s3Client;
     private final S3StorageProperties properties;
+
 
     public UUIDFileStorageS3(S3Client s3Client, S3StorageProperties properties) {
         this.s3Client = s3Client;
@@ -69,24 +77,77 @@ public class UUIDFileStorageS3 implements IUuidFileStorage {
 
     @Override
     public Path read(UUID uuid) throws IOException {
-        Path target = Files.createTempFile("photone_", "_" + uuid);
-        target.toFile().deleteOnExit();
-        s3Client.getObject(
-                req -> req
-                        .bucket(properties.getBucket())
-                        .key(uuid.toString()),
-                target
+
+        Path targetFile = Files.createTempFile(
+                "photone_", // Préfixe
+                "_" + uuid + "_" + UUID.randomUUID() + ".tmp"
         );
-        return target;
+
+        targetFile.toFile().deleteOnExit();
+
+        downloadFile(uuid, targetFile);
+
+        return targetFile;
     }
+
+    private void downloadFile(UUID uuid, Path targetFile) throws IOException {
+
+        try (OutputStream outputStream = Files.newOutputStream(targetFile)) { // Prepare the outputStream
+            // AWS download request
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(properties.getBucket())
+                    .key(uuid.toString())
+                    .build();
+
+            // Write the downloaded file into the temp file
+            s3Client.getObject(getObjectRequest, ResponseTransformer.toOutputStream(outputStream));
+
+        } catch (Exception e) {
+            Files.deleteIfExists(targetFile);
+            throw new IOException("Failed to download file from S3: " + uuid, e);
+        }
+    }
+
 
     @Override
     public void delete(UUID uuid) throws IOException {
 
     }
 
-    @Override
-    public void deleteAll() throws IOException {
 
+    @Override
+    public void deleteAll() {
+        deleteAllTempFiles();
+        deleteAllInBucket();
     }
+
+    @Profile("dev")
+    private void deleteAllInBucket() {
+        log.warn("Deleting all BUCKET files!");
+        var s3Objects = s3Client.listObjectsV2(req->req.bucket(properties.getBucket()));
+        var streamedObjects = s3Objects.contents().stream();
+        List<ObjectIdentifier> keys = streamedObjects.map(s3Object -> ObjectIdentifier.builder().key(s3Object.key()).build()).toList();
+        DeleteObjectsRequest request = DeleteObjectsRequest.builder().bucket(properties.getBucket()).delete(delete -> delete.objects(keys)).build();
+        s3Client.deleteObjects(request);
+    }
+
+    private static void deleteAllTempFiles() {
+        log.warn("Deleting all temp files!");
+        Path tmpDir = Path.of(System.getProperty("java.io.tmpdir"));
+
+        try (Stream<Path> files = Files.list(tmpDir)) {
+            files
+                    .filter(path -> path.getFileName().toString().startsWith("photone_"))
+                    .forEach(path -> {
+                        try {
+                            Files.deleteIfExists(path);
+                        } catch (IOException e) {
+                            log.warn("Could not delete temp file: {}", path, e);
+                        }
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to clean temp directory", e);
+        }
+    }
+
 }
