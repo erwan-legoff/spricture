752801c326fe6bdfb91cf2ed62aaea13adf7e650
erwil <rw4nit0@gmail.com>
2025-06-05 14:59:44 +0200

    some cleaning

--- src/main/java/fr/erwil/Spricture/Tools/FileStorage/S3/UuidFileStorageUrlS3.java
+++ src/main/java/fr/erwil/Spricture/Tools/FileStorage/S3/UuidFileStorageUrlS3.java
@@ -24,7 +24,6 @@ public class UuidFileStorageUrlS3 implements IUuidFileStorageUrl {
         return fileStorage.save(file, uuid);
     }
 
-    // TODO : Passer en String :)
     @Override
     public URL getLink(UUID uuid) {
         return linkSigner.createPresignedGetUrl(properties.getBucket(),uuid.toString());
