ab376b78265d7bb680296f6e9fbecd4cb33d3a8f
erwil <rw4nit0@gmail.com>
2025-06-06 19:19:29 +0200

    adding @Configuration

--- src/main/java/fr/erwil/Spricture/Application/User/UserProperties.java
+++ src/main/java/fr/erwil/Spricture/Application/User/UserProperties.java
@@ -3,7 +3,9 @@ package fr.erwil.Spricture.Application.User;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix = "user")
 @Data
 public class UserProperties {
